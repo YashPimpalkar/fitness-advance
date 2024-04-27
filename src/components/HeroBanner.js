f945e18480d2d6d73a58a45ddc83186ca8c71067
Yash Pimpalkar <62347457+YashPimpalkar@users.noreply.github.com>
2024-04-27 21:03:05 +0530

    Update HeroBanner.js

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
 import HeroBannerImage from '../assets/images/banner.png';
-import BodyBuilder from '../assets/images/body.jpg'
-
+// import BodyBuilder from '../assets/images/body.jpg'
+import BodyBuilder from '../assets/images/bodybuilder.jpeg'
 const HeroBanner = () => (
   <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
     <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
